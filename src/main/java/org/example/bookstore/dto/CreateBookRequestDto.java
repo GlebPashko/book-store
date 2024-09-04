b45c76fe67172786bb893a0347d2253e139cea73
GlebPashko <glebpashko2004@gmail.com>
2024-09-04 16:39:18 +0300

    Edited IsbnValidator and dto validation

--- src/main/java/org/example/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/org/example/bookstore/dto/CreateBookRequestDto.java
@@ -1,6 +1,7 @@
 package org.example.bookstore.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
@@ -8,9 +9,9 @@ import org.example.bookstore.validation.Isbn;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
+    @NotBlank
     private String title;
-    @NotNull
+    @NotBlank
     private String author;
     @Isbn
     private String isbn;
--- src/main/java/org/example/bookstore/validation/IsbnValidator.java
+++ src/main/java/org/example/bookstore/validation/IsbnValidator.java
@@ -2,55 +2,18 @@ package org.example.bookstore.validation;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import java.util.regex.Pattern;
 
 public class IsbnValidator implements ConstraintValidator<Isbn, String> {
+    private static final String ISBN_10_REGEX = "^\\d{9}[0-9X]$";
+    private static final String ISBN_13_REGEX = "^97[89]\\d{10}$";
+
     @Override
     public boolean isValid(String isbn, ConstraintValidatorContext constraintValidatorContext) {
         if (isbn == null || isbn.isEmpty()) {
             return false;
         }
 
-        return switch (isbn.length()) {
-            case 10 -> isValidIsbn10(isbn);
-            case 13 -> isValidIsbn13(isbn);
-            default -> false;
-        };
-    }
-
-    private boolean isValidIsbn10(String isbn) {
-        int sum = 0;
-        for (int i = 0; i < 9; i++) {
-            if (!Character.isDigit(isbn.charAt(i))) {
-                return false;
-            }
-            sum += (isbn.charAt(i) - '0') * (10 - i);
-        }
-
-        char checkDigit = isbn.charAt(9);
-        if (checkDigit != 'X' && !Character.isDigit(checkDigit)) {
-            return false;
-        }
-
-        sum += (checkDigit == 'X') ? 10 : (checkDigit - '0');
-        return sum % 11 == 0;
-    }
-
-    private boolean isValidIsbn13(String isbn) {
-        int sum = 0;
-        for (int i = 0; i < 12; i++) {
-            if (!Character.isDigit(isbn.charAt(i))) {
-                return false;
-            }
-            int digit = isbn.charAt(i) - '0';
-            sum += (i % 2 == 0) ? digit : digit * 3;
-        }
-
-        int checkDigit = isbn.charAt(12) - '0';
-        int calculatedCheckDigit = 10 - (sum % 10);
-        if (calculatedCheckDigit == 10) {
-            calculatedCheckDigit = 0;
-        }
-
-        return checkDigit == calculatedCheckDigit;
+        return Pattern.matches(ISBN_10_REGEX, isbn) || Pattern.matches(ISBN_13_REGEX, isbn);
     }
 }
