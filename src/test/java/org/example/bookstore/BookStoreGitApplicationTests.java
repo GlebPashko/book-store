826cab12513dbcbfe0206b3d80af9d8a3f7fff60
GlebPashko <glebpashko2004@gmail.com>
2024-11-01 13:00:19 +0200

    commit

---
+++ src/test/java/org/example/bookstore/BookStoreGitApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class BookStoreGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
