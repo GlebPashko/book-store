6ce2fb4f1193f7a2fa9035cd30d23a77a15e5ebd
GlebPashko <glebpashko2004@gmail.com>
2024-11-01 11:05:30 +0200

    commit

--- src/main/java/org/example/bookstore/model/Category.java
+++ src/main/java/org/example/bookstore/model/Category.java
@@ -9,11 +9,13 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Getter
 @Setter
 @Entity
 @SQLDelete(sql = "UPDATE categories SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted = false")
 @Table(name = "categories")
 public class Category {
     @Id
